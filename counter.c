79de7a8c75c86c4cf855601ebdfa99311c4fd49d
ScarWar <ariael2009@gmail.com>
2017-05-22 16:46:23 +0300

    New stuff

--- counter.c
+++ counter.c
@@ -24,13 +24,14 @@ int int_log10(double n) {
 }
 
 int main(int argc, const char **argv) {
-    char *filename, *pipe_file_name;
+    char *filename, pipe_file_name[strlen(PIPE_NAME_PREFIX) + 20];
     const char *trgt_c;
     off_t file_offset;
-    int length, i, R = 0, fd, pid_size;
+    int length, i, fd, pid_size;
+    long  R = 0;
 
     if (argc != 5) {
-        printf("sadasd\n");
+        printf("Error - Invalid number of arguments \n");
         return -1;
     }
 
@@ -47,52 +48,35 @@ int main(int argc, const char **argv) {
         if (*(p_void + i) == *trgt_c)
             R++;
 
-
     /* Calculate pipe name */
     pid_t child_pid = getpid();
-    pid_size = int_log10(pid_size);
-    pipe_file_name = (char *) malloc(strlen(PIPE_NAME_PREFIX) + pid_size + 1);
-    pipe_file_name[strlen(PIPE_NAME_PREFIX) + pid_size] = 0;
     sprintf(pipe_file_name, "%s%d", PIPE_NAME_PREFIX, child_pid);
 
     /* Create the pipe */
     if (mkfifo(pipe_file_name, O_RDWR) != 0) {
         // TODO error
-        printf("sdsdasdsadasdasd\n");
+        printf("Error in mkfifo\n");
         return errno;
     }
-
-    /* Send result through pipe */
-    int int_size = int_log10(R);
-    char *buffer = (char *) malloc((int_size + 1) * sizeof(char));
-    if (!buffer) {
-        // TODO error message
-        printf("asdasds\n");
-        return errno;
-    }
-    buffer[int_size] = 0;
-    sprintf(buffer, "%d", R);
-    printf("[Debug] - %d %s\n", R, buffer);
 
     /* Send signal to parent process */
-
-    // kill(getppid(), SIGUSR1);
-
-    /* Write to parent */
+     kill(getppid(), SIGUSR1);
+    
+    /*      Send result through pipe        */
+    /*          Write to parent             */
     int pipe_fd = open(pipe_file_name, O_WRONLY, S_IWUSR | S_IRUSR);
-    write(pipe_fd, buffer, (size_t) int_size + 1); // Write buffer to pipe
+    write(pipe_fd, &R, sizeof(long)); // Write buffer to pipe
 
     /* Sleep */
-    // sleep(1);
+     sleep(1);
 
     /* Free resources */
-    if (munmap(p_void, length) != 0) {
+    if (munmap(p_void, (size_t) length) != 0) {
         // TODO error
         return errno;
     }
     close(pipe_fd); // Close pipe write
     unlink(pipe_file_name);
-    free(buffer);
     free(pipe_file_name);
     return 0;
 }
--- dispatcher.c
+++ dispatcher.c
@@ -1,92 +1,116 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#include <errno.h>
+ #include <sys/wait.h>
 
-#define SQ(x) ((x) * (x))
+
 #define abs(x) ((x < 0) ? (-x) : (x))
-#define min(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define max(x, y) ((x) > (y) ? (x) : (y))
 #define PIPE_NAME_PREFIX "/tmp/counter_"
 
+long total = 0;
 
-int int_log10(double n) {
+int int_log_base(double n, int base) {
     int l = 0;
     while (n >= 1) {
         l++;
-        n /= 10;
+        n /= base;
     }
     return l;
 }
 
-
-int int_sqrt(int n) {
-    int upperBound = n, lowerBound = 0;
-    int i = 0;
-    if (n < 0) return -1;
-    if (n == 1) return 1;
-    while (upperBound - lowerBound > 1) {
-        i = (upperBound + lowerBound) >> 1;
-        if (i * i > n)
-            upperBound = i;
-        else
-            lowerBound = i;
-    }
-    return lowerBound;
-}
-
-int get_nforks(long long x) {
+int get_nforks(int x) {
     /*  Linear function such that 1K will get 1 fork
         and 4GiB and more will get 16 forks         */
-    long long y;
-    y = (5 * x) / 1431655424 + 1398096 / 1398101;
-    return (int) (1 > min(y, 16) ? 1 : min(y, 16));
+    if (x < 2 * getpagesize()) {
+        return 1;
+    }
+    int y = int_log_base(x, 4);
+    return (int) max(min(16, y), 1);
+}
+
+void my_signal_handler(int signum, siginfo_t *info, void *ptr) {
+    long R;
+    char pipe_file_name[strlen(PIPE_NAME_PREFIX) + 20];
+    printf("Signal sent from process %lu\n", (unsigned long) info->si_pid);
+    /* Calculate pipe name */
+    pid_t child_pid = info->si_pid;
+    sprintf(pipe_file_name, "%s%d", PIPE_NAME_PREFIX, child_pid); 
+    int pipe_fd = open(pipe_file_name, O_RDONLY, S_IWUSR | S_IRUSR);
+    if(pipe_fd < 0){
+        // TODO error message
+        return;
+    }
+    ssize_t _n__read = read(pipe_fd, &R, sizeof(long));
+    if (_n__read < 0 || _n__read != sizeof(long)) {
+        return;
+    }
+    total += R;
+    printf("Signal handler finished\n");
 }
 
 int main(int argc, const char **argv) {
-    int pipe_size, pid_size, ret = -2;
-    char *pipe_file_name, *buffer;
-    char *execv_argv[6] = {
-            "counter.o",
-            "s",
-            "some.txt",
-            "0",
-            "64",
-            NULL
-    };
-    pid_t p = fork();
-    if (p > 0) {
-        sleep(1);
-        pid_size = int_log10(p);
-        pipe_file_name = (char *) malloc(strlen(PIPE_NAME_PREFIX) + pid_size + 1);
-        pipe_file_name[strlen(PIPE_NAME_PREFIX) + pid_size] = 0;
-        sprintf(pipe_file_name, "%s%d", PIPE_NAME_PREFIX, p);
-        int fd = open(pipe_file_name, O_RDONLY, S_IRUSR);
-        struct stat sb;
-        stat(pipe_file_name, &sb);
-        buffer = malloc((size_t) sb.st_size);
-        printf("[Debug] - tmp counter size %d\n", (int) sb.st_size);
-        if (!buffer) {
-            printf("%s\n", "[Info] - malloc error");
-        }
-        read(fd, buffer, (size_t) sb.st_size);
-        printf("%s\n", buffer);
-        close(fd);
-    } else {
-        ret = execv("counter", execv_argv);
+    int     pipe_size, pid_size, n_forks, ret = 0;
+    char    file_offset[20], chunk_length[20];
+    long length;
+    // Structure to pass to the registration syscall
+    struct sigaction new_action;
+    memset(&new_action, 0, sizeof(new_action));
+    // Assign pointer to our handler function
+    new_action.sa_handler   = my_signal_handler;
+    // Setup the flags
+    new_action.sa_flags     = SA_SIGINFO;
+
+    /* Calculate length and number of forks */
+    struct  stat sb;
+    off_t   file_size;
+    stat(argv[2], &sb);
+    file_size    = sb.st_size;
+    n_forks      = get_nforks((long) file_size);
+    length       = ((long) file_size) / n_forks;
+    long *chunks = (long *) malloc(n_forks);
+    if (!chunks) {
+        // TODO error message
+        printf("Error - memory allocation failed \n");
+        return errno;
     }
-    printf("%d\n", ret);
+    int k;
+    for (k = 0; k < n_forks - 1; ++k) {
+        chunks[k]  = length;
+        file_size -= length;
+    }
+    chunks[k] =  file_size;
 
-
-//    s = int_sqrt(213 >> 23);
-//    s1 = s;
-//    while (s1 > 0) {
-//        s1 -= s;
-//        fork();
-//    }
+    pid_t p;
+    off_t offset = 0;
+    for (int i = 0; i < n_forks; ++i) {
+        offset += length;
+        /* Calculate execv_argv */
+        sprintf(file_offset, "%ld", offset); // Convert offset to string
+        sprintf(chunk_length, "%ld" ,chunks[i]);
+        char *execv_argv[6] = {
+            "./counter.o",
+            (char *) argv[1],
+            (char *) argv[2],
+            file_offset,
+            chunk_length,
+            NULL
+        };
+        sleep(1);
+        p = fork();
+        if(p == 0)
+            ret = execv("counter", execv_argv);
+    }
+    int status = -1;
+    while(wait(&status) < 0);
+    printf("Total number of charecters %ld\n", total);
     return 0;
 }
